cd61d36e8f6e349359beca46a2a066bf6af01473
Matt Klein <mklein246@gmail.com>
2020-02-29 23:13:26 -0800

    cleaned up styling for layer wrapper

--- src/components/Layer.js
+++ src/components/Layer.js
@@ -1,24 +1,31 @@
-import React, { useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
 import { useDispatch } from "react-redux";
-import { updateLayerData } from '../actions'
+import { updateLayerData } from "../actions";
 
 import draw from "../reducers/drawingReducer.js";
 import manipulate from "../reducers/manipulateReducer.js";
 
+const LayerWrapperSC = styled.div`
+  position: absolute;
+  width: ${props => props.width};
+  height: ${props => props.height};
+  overflow: hidden;
+`;
+
 const LayerSC = styled.canvas`
   position: absolute;
-  visibility: ${props => props.hidden ? "hidden" : "visible"};
+  visibility: ${props => (props.hidden ? "hidden" : "visible")};
   width: 300%;
   height: 300%;
   bottom: -100%;
   right: -100%;
   image-rendering: pixelated;
   z-index: ${props => props.index};
-`
+`;
 
 function Layer(props) {
-  const canvasRef = useRef(null)
+  const canvasRef = useRef(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -26,28 +33,31 @@ function Layer(props) {
     const ctx = canvas.getContext("2d");
     let queue = props.queue;
     if (queue === null) return;
-    if (queue.type === "draw") drawHandler(ctx, queue)
-    if (queue.type === "manipulate") manipulateHandler(ctx, queue)
+    if (queue.type === "draw") drawHandler(ctx, queue);
+    if (queue.type === "manipulate") manipulateHandler(ctx, queue);
   }, [props.data, props.queue, props.id]);
 
   function drawHandler(ctx, queue) {
     draw(ctx, queue);
-    dispatch(updateLayerData(props.id, canvasRef.current))
+    dispatch(updateLayerData(props.id, canvasRef.current));
   }
 
   function manipulateHandler(ctx, queue) {
     manipulate(ctx, queue);
-    dispatch(updateLayerData(props.id, canvasRef.current))
+    dispatch(updateLayerData(props.id, canvasRef.current));
   }
 
-  return <div style={{
-    position: "absolute",
-    width: props.width,
-    height: props.height,
-    overflow: "hidden",
-  }}>
-    <LayerSC width={props.width * 3} height={props.height * 3} hidden={props.hidden} index={props.index} ref={canvasRef} />
-  </div>
+  return (
+    <LayerWrapperSC width={props.width} height={props.height}>
+      <LayerSC
+        width={props.width * 3}
+        height={props.height * 3}
+        hidden={props.hidden}
+        index={props.index}
+        ref={canvasRef}
+      />
+    </LayerWrapperSC>
+  );
 }
 
-export default React.memo(Layer)
+export default React.memo(Layer);
