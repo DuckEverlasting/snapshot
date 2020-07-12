a5bce08266c0845072f189f10041cf3ef62535aa
Matt Klein <mklein246@gmail.com>
2020-07-11 23:24:35 -0700

    made some cursors!

--- src/utils/cursors.js
+++ src/utils/cursors.js
@@ -1,23 +1,38 @@
 import pencilImg from "../cursors/pencil.png";
 import dropperImg from "../cursors/dropper.png";
 import crosshairsImg from "../cursors/crosshairs.png";
+import smallArrowImg from "../cursors/small_arrow.png";
+import smallArrowPlusImg from "../cursors/small_arrow_plus.png";
+import smallArrowMinusImg from "../cursors/small_arrow_minus.png";
+import smallArrowCrossImg from "../cursors/small_arrow_cross.png";
 
 export default function getCursor(cursorName, keys) {
+  function getSelectionArrow() {
+    if (keys.alt && keys.shift) {
+      return smallArrowCrossImg
+    } else if (keys.shift) {
+      return smallArrowPlusImg
+    } else if (keys.alt) {
+      return smallArrowMinusImg
+    } else {
+      return smallArrowImg
+    }
+  }
   switch (cursorName) {
     case "pencil":
       return `url(${pencilImg}) -22 22, auto`;
     case "line":
-      return "crosshair";
+      return `url(${smallArrowImg}) 0 0, auto`;
     case "fillRect":
-      return "crosshair";
+      return `url(${smallArrowImg}) 0 0, auto`;
     case "drawRect":
-      return "crosshair";
+      return `url(${smallArrowImg}) 0 0, auto`;
     case "selectRect":
-      return "crosshair";
+      return `url(${getSelectionArrow()}) 0 0, auto`;
     case "selectEllipse":
-      return "crosshair";
+      return `url(${getSelectionArrow()}) 0 0, auto`;
     case "lasso":
-      return "crosshair";
+      return `url(${getSelectionArrow()}) 0 0, auto`;
     case "stamp":
       return keys.alt ? `url(${crosshairsImg}) 10 10, auto` : "crosshair";
     case "eyeDropper":
